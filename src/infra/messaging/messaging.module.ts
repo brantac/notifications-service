c06f8e429bb5a0d0b13b34a9af56861218fd7ff2
Arthur Brant <arthurcl.br@hotmail.com>
2022-12-20 13:53:33 -0300

    inject NotificationsController to MessagingModule

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { NotificationsController } from './kafka/controllers/notifications.controller';
 import { KafkaConsumerService } from './kafka/kafka-consumer.service';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [NotificationsController],
   providers: [KafkaConsumerService],
 })
 export class MessagingModule {}
