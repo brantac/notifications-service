ff32c67c56734c813cf0dbdbeafdf19f5649800e
Arthur Brant <arthurcl.br@hotmail.com>
2022-12-20 10:53:15 -0300

    create class MessagingModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from './infra/http/http.module';
 import { DatabaseModule } from './infra/database/database.module';
+import { MessagingModule } from '@infra/messaging/messaging.module';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [HttpModule, DatabaseModule, MessagingModule],
 })
 export class AppModule {}
