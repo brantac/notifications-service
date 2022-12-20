bcf5cb11b2836b7a38d156fd1d2c055c4c583f32
Arthur Brant <arthurcl.br@hotmail.com>
2022-12-20 17:14:38 -0300

    import DatabaseModule and SendNotification provider to MessagingModule

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,10 +1,12 @@
+import { SendNotification } from '@app/use-cases/send-notification';
+import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './kafka/controllers/notifications.controller';
 import { KafkaConsumerService } from './kafka/kafka-consumer.service';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [KafkaConsumerService],
+  providers: [KafkaConsumerService, SendNotification],
 })
 export class MessagingModule {}
